6e05061198aa60b43212f8ba72c5a4a6d15d4f84
Frank Reimann <frank.reimann@jambit.com>
2023-02-06 11:48:20 +0100

    .

---
+++ dist/test.js
@@ -0,0 +1,54 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var __param = (this && this.__param) || function (paramIndex, decorator) {
+    return function (target, key) { decorator(target, key, paramIndex); }
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.InMemoryCrudService = void 0;
+require("reflect-metadata");
+const typedi_1 = require("typedi");
+require("./di.generated");
+const di_utils_1 = __importDefault(require("./di-utils"));
+const users = {};
+// (2) the service that implements the interface and that gets injected
+let InMemoryCrudService = class InMemoryCrudService {
+    create(t) {
+        users[t.id] = t;
+    }
+    read(id) {
+        return users[id];
+    }
+};
+InMemoryCrudService = __decorate([
+    (0, di_utils_1.default)() // { extends: CrudInterface }
+    ,
+    (0, typedi_1.Service)()
+], InMemoryCrudService);
+exports.InMemoryCrudService = InMemoryCrudService;
+// (3) the service that uses the interface
+let Application = class Application {
+    constructor(databaseService) {
+        this.databaseService = databaseService;
+    }
+};
+Application = __decorate([
+    (0, typedi_1.Service)(),
+    __param(0, (0, typedi_1.Inject)()),
+    __metadata("design:paramtypes", [Object])
+], Application);
+// (4) the result
+const instance = typedi_1.Container.get(Application);
+instance.databaseService.create({ id: 1, name: "Frank" });
+const user = instance.databaseService.read(1);
+console.log(user);
---
+++ src/test.ts
@@ -0,0 +1,38 @@
+import "reflect-metadata";
+import { Container, Inject, Service } from "typedi";
+import "./di.generated";
+import { User } from "./User";
+import AutoWire from "./di-utils";
+
+const users: Record<number, User> = {};
+
+// (1) the interface
+export interface CrudInterface<T extends { id: number }> {
+  create: (t: T) => void;
+  read: (id: number) => T;
+}
+
+// (2) the service that implements the interface and that gets injected
+@AutoWire() // { extends: CrudInterface }
+@Service()
+export class InMemoryCrudService implements CrudInterface<User> {
+  create(t: User) {
+    users[t.id] = t;
+  }
+
+  read(id: number) {
+    return users[id];
+  }
+}
+
+// (3) the service that uses the interface
+@Service()
+class Application {
+  constructor(@Inject() public databaseService: CrudInterface<User>) {}
+}
+
+// (4) the result
+const instance = Container.get(Application);
+instance.databaseService.create({ id: 1, name: "Frank" });
+const user = instance.databaseService.read(1);
+console.log(user);
